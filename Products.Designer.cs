1ec9033f563c55584a02c60eb6081f7de69548c6
Marcus <rinkajhin@gmail.com>
2024-12-31 19:33:10 +0800

    delete product

---
+++ Products.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Paquito_sPizzeria
+{
+    partial class Products
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.productGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.editBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.productGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // productGridView
+            // 
+            this.productGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.productGridView.Location = new System.Drawing.Point(53, 73);
+            this.productGridView.Name = "productGridView";
+            this.productGridView.RowHeadersWidth = 51;
+            this.productGridView.RowTemplate.Height = 24;
+            this.productGridView.Size = new System.Drawing.Size(595, 242);
+            this.productGridView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Products";
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(53, 346);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(118, 51);
+            this.addBtn.TabIndex = 2;
+            this.addBtn.Text = "add ";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(196, 346);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(118, 51);
+            this.deleteBtn.TabIndex = 3;
+            this.deleteBtn.Text = "delete ";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // editBtn
+            // 
+            this.editBtn.Location = new System.Drawing.Point(344, 346);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(118, 51);
+            this.editBtn.TabIndex = 4;
+            this.editBtn.Text = "edit";
+            this.editBtn.UseVisualStyleBackColor = true;
+            // 
+            // Products
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.productGridView);
+            this.Name = "Products";
+            this.Text = "Products";
+            this.Load += new System.EventHandler(this.Products_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.productGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView productGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button editBtn;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Paquito_sPizzeria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Admin());
+            Application.Run(new Products());
         }
     }
 }
