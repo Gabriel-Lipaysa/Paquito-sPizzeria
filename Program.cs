eb839017d0b0ca0723b0f618fcfac7397d43e107
rinkaji <rinkajhin@gmail.com>
2025-01-04 14:32:02 +0800

    orders filter and search

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Paquito_sPizzeria
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new orders());
         }
     }
 }
